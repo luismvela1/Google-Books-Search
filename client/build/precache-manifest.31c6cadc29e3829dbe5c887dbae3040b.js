a885c1c3dfa3f487f311a0962c2379d498ef5314
Luis Vela <luismvela1@aol.com>
2021-03-30 23:53:02 -0500

    added client/build files

---
+++ client/build/precache-manifest.31c6cadc29e3829dbe5c887dbae3040b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5147da41a6c506a648e9bcf19a23de9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "203aa1bce0570c7269f6",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "c85d5ec36e5896b35d48",
+    "url": "/static/js/2.a1a48849.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a1a48849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203aa1bce0570c7269f6",
+    "url": "/static/js/main.daba9d2d.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
